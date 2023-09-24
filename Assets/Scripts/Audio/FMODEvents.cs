2960ab64780117eec3f08675c46facee48c6086f
Xavi <l4sixx@gmail.com>
2023-09-25 01:06:55 +0200

    FMOD Implementation

---
+++ Assets/Scripts/Audio/FMODEvents.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class FMODEvents : MonoBehaviour
+{
+    [field: Header("Button Menu SFX")]
+    [field: SerializeField] public EventReference buttonMenu { get; private set; }
+    public static FMODEvents instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
