eb43a628266e376d08f327e0cccdc57b61a08b68
victorcano4 <victor.cano.rodriguez4@gmail.com>
2023-09-24 05:23:26 +0200

    Revert "Update Client.cs"
    This reverts commit aabcab52ea5fe6cb24ac6056b1708862943f7538.

--- Assets/Scripts/Game Test/Models/Client.cs
+++ Assets/Scripts/Game Test/Models/Client.cs
@@ -9,11 +9,7 @@ using UnityEngine.SceneManagement;
 public class Client : ScriptableObject
 {
     public string Name;
-    public string Description;
-    //TODO: todas estas listas pueden tener cosas que NO se quieran, as? que hay que crear strcuts con propiedad objeto y un bool que dice si se quiere o no
-    public List<IngredientOrder> WantedIngredients;
-    public List<IngredientPropertyOrder> WantedIngredientProperties;
-    public List<DrinkOrder> WantedDrinks;
+    public List<Drink> WantedDrinks;
     public List<string> DialogueLines;
     public Sprite Sprite;
 }
