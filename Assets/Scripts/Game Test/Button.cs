f463e941b19176cfde388c2aa986cb9907f5d0f2
victorcano4 <victor.cano.rodriguez4@gmail.com>
2023-09-25 07:04:38 +0200

    progress marks

--- Assets/Scripts/Game Test/Button.cs
+++ Assets/Scripts/Game Test/Button.cs
@@ -6,13 +6,25 @@ using UnityEngine;
 public class Button : MonoBehaviour
 {
     public CustomerManager customerManager;
+    public Sprite successSprite;
+    public Sprite failSprite;
+
+    private int orderNumber = 1;
 
     public void TaskOnClick()
     {
         bool orderCorrect = customerManager.ProcessOrder();
         if (orderCorrect)
+        {
             Debug.Log("Order correct! :D");
+            GameObject.Find("Order" + orderNumber.ToString()).GetComponent<UnityEngine.UI.Image>().sprite = successSprite;
+        }            
         else
+        {
             Debug.Log("Order not correct! D:");
+            var currentOrderMark = GameObject.Find("Order" + orderNumber.ToString());
+            currentOrderMark.GetComponent<UnityEngine.UI.Image>().sprite = failSprite;
+        }
+        orderNumber++;
     }
 }
